25bf4c60d8b581db2c2126021e2679d38859853d
HuMiaoxin666 <humiaoxin@gmail.com>
2018-10-18 16:44:05 +0800

    Another type of axis !

--- public/js/my/lineChart.js
+++ public/js/my/lineChart.js
@@ -34,53 +34,122 @@ var lineChart = (function () {
         }
 
         //设置坐标轴
-        let xScale = d3.scaleLinear().domain([min_arr[0], max_arr[0]]).range([0, svg_width * 0.85])
-        let x_axis = d3.axisBottom(xScale).ticks(5).tickPadding(5).tickSize(5);
-        mapView.svg_lineChart.append('g')
-            .attr("class", "axis")
-            .attr("transform", "translate(40," + svg_height * 0.45 + ")")
-            .call(x_axis)
-        let yScale_arr = [], yAxis_arr = [];
-        for (let i = 1; i < min_arr.length; i++) {
-            if (max_arr[i] < 0) {
-                let tmp_ys = d3.scaleLinear().domain([-max_arr[i] * 1.5, max_arr[i] * 1.5]).range([0, svg_height * 0.7])
-                yScale_arr.push(tmp_ys);
-                yAxis_arr.push(d3.axisLeft(tmp_ys).ticks(5).tickPadding(5).tickSize(5));
-            } else if (max_arr[i] > 0) {
-                let tmp_ys = d3.scaleLinear().domain([max_arr[i] * 1.5, -max_arr[i] * 1.5]).range([0, svg_height * 0.7])
-                yScale_arr.push(tmp_ys);
-                yAxis_arr.push(d3.axisLeft(tmp_ys).ticks(5).tickPadding(5).tickSize(5));
-            }else if(max_arr[i] == min_arr[i] && max_arr[i] == 0){
-                let tmp_ys = d3.scaleLinear().domain([-50, 50]).range([0, svg_height * 0.7]);
-                yScale_arr.push(tmp_ys);
-                yAxis_arr.push(d3.axisLeft(tmp_ys).ticks(5).tickPadding(5).tickSize(5));
-            }
-        }
-        // let yScale = d3.scaleLinear().domain([100, 0]).range([0, svg_height * 0.7])
+        let yScale = d3.scaleLinear().domain([min_arr[0], max_arr[0]]).range([0, svg_height * 0.85])
+        let y_axis = d3.axisLeft(yScale).tickPadding(5).tickSize(5);
         mapView.svg_lineChart.append('g')
             .attr("class", "axis")
             .attr("transform", "translate(40," + svg_height * 0.1 + ")")
-            .attr("id",'y_axis')
-            .call(yAxis_arr[0])
-
+            .call(y_axis)
+        let xScale_arr = [],
+            xAxis_arr = [];
+        for (let i = 1; i < min_arr.length; i++) {
+            if(max_arr[i] > 0 && min_arr[i] <= 0){
+                let tmp_ys = d3.scaleLinear().domain([min_arr[i] * 1.2, max_arr[i] * 1.2]).range([0, svg_width * 0.16])
+                xScale_arr.push(tmp_ys);
+            }else if(max_arr[i] < 0 && min_arr[i] <= 0){
+                let tmp_ys = d3.scaleLinear().domain([min_arr[i] * 1.2, max_arr[i] * 0.8]).range([0, svg_width * 0.16])
+                xScale_arr.push(tmp_ys);
+            }else if(max_arr[i] > 0 && min_arr[i] > 0){
+                let tmp_ys = d3.scaleLinear().domain([min_arr[i] * 0.8, max_arr[i] * 1.2]).range([0, svg_width * 0.16])
+                xScale_arr.push(tmp_ys);
+            }else{
+                let tmp_ys = d3.scaleLinear().domain([min_arr[i] * 0.8, max_arr[i] * 0.8]).range([0, svg_width * 0.16])
+                xScale_arr.push(tmp_ys);
+            }
+          
+        }
+        //画属性分界线
 
         let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+        let areaLine = d3.line().x(function (d) {
+            return d[0];
+        }).y(function (d) {
+            return d[1];
+        })
+        let line_arr = [
+            [35, svg_height * 0.1],
+            [40 + svg_width * 0.8, svg_height * 0.1]
+        ];
         var text_loc = [];
-        for(let i =1 ; i< 6; i++){
-            let tmp_loc = [svg_width * 0.1 * i , svg_height * 0.9];
+        mapView.svg_lineChart.append("path")
+            .attr("d", areaLine(line_arr))
+            .attr("stroke", "black")
+            .attr("stroke-width", 1)
+            .attr("fill", 'none')
+            .attr("id", "x_axis");
+        for (let i = 1; i < 6; i++) {
+            let tmp_loc = [35 + svg_width * 0.05+ svg_width * 0.16 * (i - 1), svg_height * 0.1];
             text_loc.push(tmp_loc);
+            let tmp_sp = [40 + svg_width * 0.16 * i, svg_height * 0.1],
+                tmp_ep = [40 + svg_width * 0.16 * i, svg_height * 0.95];
+            let tmp_line = [tmp_sp, tmp_ep];
+            mapView.svg_lineChart.append("path")
+                .attr("d", areaLine(tmp_line))
+                .attr("stroke", "black")
+                .attr("stroke-width", 1)
+                .attr("fill", 'none')
+                .attr("id", "x_axis");
+
         }
-        
+        //属性标识
+        mapView.svg_lineChart.append("a").selectAll("text").data(text_loc)
+            .enter().append("text")
+            .attr("transform", function (d) {
+                return "translate(" + d[0] + ',' + d[1] + ")";
+            }).attr("font-size", 10)
+            .style("stroke", function (d, i) {
+                return colorScale(i + 1);
+            })
+            .text(function (d, i) {
+                console.log();
+                return data.basic_attr[i + 1];
+            }).on("click", function (d, i) {
+                for(let i = 0; i < 5; i++){
+                    d3.select("#"+data.basic_attr[i+1]).attr("stroke-width",0.5).attr("opacity",0.5);
+                }
+                d3.select("#"+ data.basic_attr[i+1]).attr("stroke-width",2).attr("opacity",1.0);
+                // d3.select("#y_axis").call(yAxis_arr[i]);
+            })
+
+        // let yScale_arr = [], yAxis_arr = [];
+        // for (let i = 1; i < min_arr.length; i++) {
+        //     if (max_arr[i] < 0) {
+        //         let tmp_ys = d3.scaleLinear().domain([-max_arr[i] * 1.5, max_arr[i] * 1.5]).range([0, svg_height * 0.7])
+        //         yScale_arr.push(tmp_ys);
+        //         yAxis_arr.push(d3.axisLeft(tmp_ys).ticks(5).tickPadding(5).tickSize(5));
+        //     } else if (max_arr[i] > 0) {
+        //         let tmp_ys = d3.scaleLinear().domain([max_arr[i] * 1.5, -max_arr[i] * 1.5]).range([0, svg_height * 0.7])
+        //         yScale_arr.push(tmp_ys);
+        //         yAxis_arr.push(d3.axisLeft(tmp_ys).ticks(5).tickPadding(5).tickSize(5));
+        //     }else if(max_arr[i] == min_arr[i] && max_arr[i] == 0){
+        //         let tmp_ys = d3.scaleLinear().domain([-50, 50]).range([0, svg_height * 0.7]);
+        //         yScale_arr.push(tmp_ys);
+        //         yAxis_arr.push(d3.axisLeft(tmp_ys).ticks(5).tickPadding(5).tickSize(5));
+        //     }
+        // }
+        // // let yScale = d3.scaleLinear().domain([100, 0]).range([0, svg_height * 0.7])
+        // mapView.svg_lineChart.append('g')
+        //     .attr("class", "axis")
+        //     .attr("transform", "translate(40," + svg_height * 0.1 + ")")
+        //     .attr("id",'y_axis')
+        //     .call(yAxis_arr[0])
+
+
+
+
         for (let i = 1; i < 6; i++) {
             console.log(i);
             let lineFun = d3.line()
                 .x(function (d) {
-                    return xScale(d[0]) + 40;
+                    console.log(xScale_arr[i - 1](d[i]) );
+                    let tmp_x = xScale_arr[i - 1](d[i]) + 40 + (svg_width * 0.16) * (i - 1);
+                    return tmp_x;
                 })
                 .y(function (d) {
-                    let tmp_y = yScale_arr[i - 1](d[i]) + svg_height * 0.1;
-                    if(tmp_y > svg_height*0.8)
-                        tmp_y = svg_height*0.83;
+                    let tmp_y = yScale(d[0]) + svg_height * 0.1;
+                    // if(tmp_y > svg_width*0.8)
+                    //     tmp_y = svg_height*0.83;
                     return tmp_y;
                 })
                 .curve(d3.curveBasis);
@@ -91,26 +160,8 @@ var lineChart = (function () {
                 .attr("fill", 'none')
                 .attr("id", data.basic_attr[i]);
         }
-        console.log(text_loc);
+        // console.log(text_loc);
 
-        mapView.svg_lineChart.append("a").selectAll("text").data(text_loc)
-            .enter().append("text")
-            .attr("transform", function (d) {
-                return "translate(" + d[0] + ',' + d[1] + ")";
-            }).attr("font-size", 10)
-            .style("stroke", function(d,i){
-                return colorScale(i+1);
-            })
-            .text(function (d, i) {
-                console.log();
-                return data.basic_attr[i + 1];
-            }).on("click",function(d,i){
-                for(let i = 0; i < 5; i++){
-                    d3.select("#"+data.basic_attr[i+1]).attr("stroke-width",0.5).attr("opacity",0.5);
-                }
-                d3.select("#"+ data.basic_attr[i+1]).attr("stroke-width",2).attr("opacity",1.0);
-                d3.select("#y_axis").call(yAxis_arr[i]);
-            })
 
     }
     return {
